fca42ff68b0085c213fa0490910e6b1f74328b86
reallytb <maks07092004@mail.ru>
2026-02-14 04:17:31 +0300

    made funcs GetUserByTelegramID, UpdateUserGrade, IsAdmin

--- internal/database/repository/user_repository.go
+++ internal/database/repository/user_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"go.uber.org/zap"
 
@@ -16,7 +17,7 @@ type UserRepository struct {
 	logger *zap.Logger
 }
 
-func (u UserRepository) CreateUser(ctx context.Context, telegramID int64, username, firstName, lastName string) (*models.User, error) {
+func (u *UserRepository) CreateUser(ctx context.Context, telegramID int64, username, firstName, lastName string) (*models.User, error) {
 	if err := ctx.Err(); err != nil {
 		u.logger.Error("context cancelled before query")
 		return nil, err
@@ -30,10 +31,6 @@ func (u UserRepository) CreateUser(ctx context.Context, telegramID int64, userna
 		return &user, nil
 	}
 	if err != sql.ErrNoRows {
-		if ctx.Err() != nil {
-			u.logger.Error("context cancelled")
-			return nil, err
-		}
 		u.logger.Error("query error", zap.Error(err))
 		return nil, err
 	}
@@ -44,5 +41,65 @@ func (u UserRepository) CreateUser(ctx context.Context, telegramID int64, userna
 		return nil, err
 	}
 	u.logger.Info("user created", zap.Int64("telegram_id", telegramID))
-	return &models.User{}, nil
+	return &user, nil
+}
+
+func (u *UserRepository) GetUserByTelegramID(ctx context.Context, telegramID int64) (*models.User, error) {
+	if err := ctx.Err(); err != nil {
+		u.logger.Error("context cancelled before query")
+		return nil, err
+	}
+	var user models.User
+	err := u.db.GetContext(ctx, &user, "SELECT * FROM users WHERE telegram_id = $1", telegramID)
+	if err != nil {
+		if ctx.Err() != nil {
+			u.logger.Error("context cancelled")
+			return nil, err
+		}
+		u.logger.Error("query error", zap.Error(err))
+		return nil, err
+	}
+	u.logger.Info("user found", zap.Int64("telegram_id", telegramID))
+	return &user, nil
+}
+
+func (u *UserRepository) UpdateUserGrade(ctx context.Context, telegramID int64, grade int) error {
+	if err := ctx.Err(); err != nil {
+		u.logger.Error("context cancelled before query")
+		return err
+	}
+	res, err := u.db.ExecContext(ctx, "UPDATE users SET grade = $1 WHERE telegram_id = $2", grade, telegramID)
+	if err != nil {
+		u.logger.Error("query error", zap.Error(err))
+		return err
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		u.logger.Error("failed to get rows affected", zap.Error(err))
+		return err
+	}
+	if rowsAffected == 0 {
+		u.logger.Error("no user found")
+		return errors.New("no user found")
+	}
+	u.logger.Info("user grade updated succesfully")
+	return nil
+}
+
+func (u *UserRepository) IsAdmin(ctx context.Context, telegramID int64) (bool, error) {
+	if err := ctx.Err(); err != nil {
+		u.logger.Error("context cancelled before query")
+		return false, err
+	}
+	var user models.User
+	err := u.db.GetContext(ctx, &user, "SELECT * FROM users WHERE telegram_id = $1", telegramID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			u.logger.Error("no user found")
+			return false, err
+		}
+		u.logger.Error("query error", zap.Error(err))
+		return false, err
+	}
+	return user.IsAdmin, nil
 }
